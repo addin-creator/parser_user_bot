55ad56968324b950d4193564095557e32b662ef4
programmist2414 <shmitov107@gmail.com>
2026-04-25 18:45:18 +0300

    Update class.py
    stylistic changes

--- class.py
+++ class.py
@@ -9,6 +9,8 @@ try:
 except RuntimeError:
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+
+
 class Scraper:
     def __init__(self, channels: list[int], api_id: int, api_hash: int, chat: str, topic_number, loop=loop):
         self.channels = channels
@@ -17,9 +19,9 @@ class Scraper:
         self.chat = chat
         self.loop = loop
         self.topic_number = topic_number
+       
     async def run(self):
         async with TelegramClient('tests', api_id=self.api_id, api_hash=self.api_hash, loop=self.loop) as client:
-           
             for i in range(len(self.channels)):
                 try:
                     unread_count = await self.get_unread_count(client, self.channels[i])
@@ -59,23 +61,26 @@ class Scraper:
                     print(f'возникла ошибка {e}, поэтому канал был переключен на следующий')
                     continue
                     
-
     async def get_unread_count(self, client:TelegramClient, channel_id: int) -> int:
         result = await client(messages.GetPeerDialogsRequest(
             peers=[channel_id]
         ))
         await asyncio.sleep(random.uniform(1.0, 3.0))
         return result.dialogs[0].unread_count
+       
     async def get_messages_id(self, messages: list)->list:
         ids = []
         for message in messages:
             if message.message and not message.reply_markup and not "подпишись" in message.message.lower() and not "подписывайся" in message.message.lower():
                 ids.append(message.id)
         return ids
+       
     async def print_logging_info(self, client: TelegramClient, channel_id: int) -> None:
         entity = await client.get_entity(channel_id)
         logging.info(f'сейчас обрабатывается канал {entity.title}')
         return 
+
+
 scr = Scraper(
     [
     channels_id
@@ -83,8 +88,9 @@ scr = Scraper(
     config.api_id.get_secret_value(),
     config.api_hash.get_secret_value(),
     config.chat.get_secret_value(),
-    7
+    7 #this is topic number, you can write your topic nubmer
     )
+
 if __name__=="__main__":
     asyncio.run(scr.run())
         
